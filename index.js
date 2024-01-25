81b28357450668c2dc8b8fea445552599e052571
Alexander Stracke <Alexander.Stracke@gmx.net>
2024-01-25 10:00:56 +0100

    task1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmarkButton = document.querySelector('[data-js="button_Bookmark"]');
+
+bookmarkButton.addEventListener("click", () => {
+    bookmarkButton.classList.toggle("bookmark--active");
+    console.log("hello");
+})
